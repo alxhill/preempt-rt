824fa36d3abcb03b3500edfa1963e0a315894eb8
Alexander Hill <me@alxhill.com>
2025-07-23 17:33:48 -0400

    docs: additional doc cleanup (#8)
    * add module docs
    
    * don't run rt code
    
    * docs: improve module docs
    
    * more doc improvements

--- src/thread.rs
+++ src/thread.rs
@@ -1,22 +1,39 @@
-use crate::sched::{IntoSchedParam, Scheduler};
+use crate::sched::{IntoSchedParams, Scheduler};
 use std::thread;
 
-pub fn spawn<F, T>(scheduler: Scheduler, param: impl IntoSchedParam, f: F) -> thread::JoinHandle<T>
+/// Spawn a thread with the provided scheduler and params.
+///
+/// Params can either be a SchedParams struct, or an i32 representing the desired priority.
+/// This function validates that the priority is between min and max for the scheduler before
+/// attempting to set it. It panics if the priority is outside the allowed range or setting the
+/// scheduler returns an error code.
+pub fn spawn<F, T>(
+    scheduler: Scheduler,
+    params: impl IntoSchedParams,
+    f: F,
+) -> thread::JoinHandle<T>
 where
     F: FnOnce() -> T,
     F: Send + 'static,
     T: Send + 'static,
 {
-    try_spawn(scheduler, param, move |set_result| {
+    try_spawn(scheduler, params, move |set_result| {
         set_result.expect("failed to set scheduler");
         f()
     })
 }
 
+/// Spawn a thread and attempt to set the schedule of the current thread. The result of setting
+/// the scheduler is provided to the thread closure as an argument.
+///
+/// Params can either be a SchedParams struct, or an i32 representing the desired priority.
+/// This function validates that the priority is between min and max for the scheduler before
+/// attempting to set it. Failures will continue execution and pass through the Result to the
+/// thread closure.
 #[cfg(target_os = "linux")]
 pub fn try_spawn<F, T>(
     scheduler: Scheduler,
-    param: impl IntoSchedParam,
+    params: impl IntoSchedParams,
     f: F,
 ) -> thread::JoinHandle<T>
 where
@@ -24,19 +41,29 @@ where
     F: Send + 'static,
     T: Send + 'static,
 {
-    let param = param.into_sched_param();
+    let params = params.into_sched_params();
     thread::spawn(move || {
         let set_result = scheduler
-            .with_priority(param.priority)
+            .with_params(params)
             .and_then(|ps| ps.set_current());
         f(set_result)
     })
 }
 
+/// Spawn a thread and attempt to set the schedule of the current thread. The result of setting
+/// the scheduler is provided to the thread closure as an argument.
+///
+/// Params can either be a SchedParams struct, or an i32 representing the desired priority.
+/// This function validates that the priority is between min and max for the scheduler before
+/// attempting to set it. Failures will continue execution and pass through the Result to the
+/// thread closure.
+///
+/// This is a stub version of the function that always passes PreemptRtError::NonLinuxPlatform
+/// to the thread closure.
 #[cfg(all(feature = "non-linux-stubs", target_os = "macos"))]
 pub fn try_spawn<F, T>(
     _scheduler: Scheduler,
-    _param: impl IntoSchedParam,
+    _params: impl IntoSchedParams,
     f: F,
 ) -> thread::JoinHandle<T>
 where
